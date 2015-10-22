c8cab069c6b1f67cbba24b1f48d6bc39e8b069b5
madbizarre <andrew-cool@bk.ru>
2015-10-22 23:36:12 +0300

    0.0.1

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -28,7 +26,6 @@ app.use(require('node-compass')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next){
-    res.render('index', {title: 'Express'});
+    res.render('index');
 });
 
-router.get('/parket', function (req, res, next){
-    res.render('parket', {title: 'Express'});
+router.get('/parquet', function (req, res, next){
+    res.render('parquet');
 });
-router.get('/parket2', function (req, res, next){
-    res.render('parket2', {title: 'Express'});
+router.get('/parquet-white', function (req, res, next){
+    res.render('parquet-white');
 });
-router.get('/parket3', function (req, res, next){
-    res.render('parket3', {title: 'Express'});
+router.get('/parquet-gray', function (req, res, next){
+    res.render('parquet-gray');
+});
+router.get('/parquet-orange', function (req, res, next){
+    res.render('parquet-orange');
+});
+router.get('/parquet-burlywood', function (req, res, next){
+    res.render('parquet-burlywood');
 });
 
 module.exports = router;
