07583002b535df37002bb2901dd5a4324a94b162
madbizarre <andrew-cool@bk.ru>
2015-10-23 00:05:23 +0300

    0.0.2

--- app.js
+++ app.js
@@ -17,21 +17,19 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(require('node-compass')({
-  mode: 'expanded',
-  project: path.join(__dirname, 'public')
+    mode: 'expanded',
+    project: path.join(__dirname, 'public')
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next){
+    res.redirect('/');
 });
 
 // error handlers
@@ -39,23 +37,19 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next){
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next){
+    res.redirect('/');
 });
 
 
